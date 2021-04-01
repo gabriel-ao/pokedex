017d23f4ed905850f57df05c1dc589853a8c70ec
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2021-04-01 11:33:17 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+<H1>Projeto de estudo</H1>
+
+<p> Projeto para criar uma front responsivo com o tema de pokemon </p>
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>pokedex gabriel</h1>
+    </div>
+  );
+}
+
+export default App;
